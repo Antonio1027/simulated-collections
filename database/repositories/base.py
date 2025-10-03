7052f959b2241aa81ab508198a6cb79c6743c05d
Antonio1027 <shilong_92@hotmail.com>
2025-10-02 23:39:34 -0600

    Feature: client repository

--- database/repositories/base.py
+++ database/repositories/base.py
@@ -1,5 +1,6 @@
 from database.connector import db
 
+
 class BaseRepository:
     def __init__(self, collection_name: str):
         self.collection = db[collection_name]
--- database/repositories/client_repository.py
+++ database/repositories/client_repository.py
@@ -1,22 +1,34 @@
 from database.repositories.base import BaseRepository
+from bson import ObjectId
+
 
 class ClientRepository(BaseRepository):
     def __init__(self):
         super().__init__("clients")
 
     async def get_by_id(self, client_id: str):
-        return await self.collection.find_one({"_id": client_id})
+        try:
+            obj_id = ObjectId(client_id)
+        except Exception:
+            return None
+        client_data = await self.collection.find_one({"_id": obj_id})
+        return client_data
 
     async def create(self, client_data: dict):
         result = await self.collection.insert_one(client_data)
         return str(result.inserted_id)
 
     async def update(self, client_id: str, update_data: dict):
-        await self.collection.update_one({"_id": client_id}, {"$set": update_data})
+        try:
+            obj_id = ObjectId(client_id)
+        except Exception:
+            return None
+        await self.collection.update_one({"_id": obj_id}, {"$set": update_data})
         return await self.get_by_id(client_id)
 
     async def delete(self, client_id: str):
-        return await self.collection.delete_one({"_id": client_id})
-
-    async def list_all(self):
-        return await self.collection.find().to_list(100)
+        try:
+            obj_id = ObjectId(client_id)
+        except Exception:
+            return None
+        return await self.collection.delete_one({"_id": obj_id})
